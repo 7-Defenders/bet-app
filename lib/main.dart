fcec1d0b03e2b6841b92235844f3468013fb9d96
Franek Suszko <franeksu@gmail.com>
2023-09-05 20:55:24 +0200

    .env/CI fix attempt v2

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env.dart");
   // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
