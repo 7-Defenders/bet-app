679c6c5340a2ecfded7e7bf55e25d64332c80e7e
Franek Suszko <franeksu@gmail.com>
2023-09-07 19:29:39 +0200

    linter build context fix attempt 2

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -161,7 +161,6 @@ class _LogInScreenState extends State<LogInScreen> {
                         ),
                       ),
                     ),
-
                   ],
                 ),
               ],
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void showSnackbarMessage(String message, BuildContext context) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(message),
-      duration: const Duration(seconds: 2),
-    ),
-  );
+  if (context.mounted) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
 }
