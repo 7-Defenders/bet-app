018ac187f780f76ca03693578756dbe05b5f2ddb
Franek Suszko <franeksu@gmail.com>
2023-12-02 15:22:31 +0100

    adding points in shop

---
+++ lib/components/shop_screen/add_points_widget.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AddPointsWidget extends StatefulWidget {
+  const AddPointsWidget({super.key});
+
+  @override
+  State<AddPointsWidget> createState() => _AddPointsWidgetState();
+}
+
+class _AddPointsWidgetState extends State<AddPointsWidget> {
+
+  Future<void> addPoints(String uid, int pointsToAdd) async {
+    DocumentReference userDocRef = FirebaseFirestore.instance.collection('Users').doc(uid);
+    final int currentPoints = await userDocRef.get().then((doc) => doc['balance'] as int);
+    final int newPoints = currentPoints + pointsToAdd;
+    await userDocRef.get().then((doc) => doc.reference.update({'balance': newPoints}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) => _buildAddPointsDialog(),
+        );
+      },
+      child: const Text('Add 100 Points'),
+    );
+  }
+
+  Widget _buildAddPointsDialog() {
+    return AlertDialog(
+      title: const Text('Add 100 Points'),
+      content: const Text('Are you sure you want to add 100 points to your account?'),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () async {
+            final String uid = FirebaseAuth.instance.currentUser!.uid;
+            await addPoints(uid, 100);
+            if (mounted) {
+              Navigator.of(context).pop();
+            }
+          },
+          child: const Text('Confirm'),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/navbar_screens/shop_screen.dart
+++ lib/screens/navbar_screens/shop_screen.dart
@@ -1,4 +1,6 @@
 import 'package:app/blocs/league_joining_bloc/league_joining_bloc.dart';
+import 'package:app/components/events_screen/join_league_widget.dart';
+import 'package:app/components/shop_screen/add_points_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,206 +14,20 @@ class ShopScreen extends StatefulWidget {
 class _ShopScreenState extends State<ShopScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
         Expanded(
           child: Column(
             children: [
-              const SizedBox(height: 50),
-              const Text('Random Text'),
-              const SizedBox(height: 20),
-              BlocConsumer<LeagueJoiningBloc, LeagueJoiningState>(
-                builder: (context, state) {
-                  if (state is LeagueJoiningLoadingState) {
-                    return _showLoadingDialog();
-                  } else if (state is LeagueJoiningInitialState) {
-                    return _buildInitialUI(context);
-                  } else if (state is LeagueJoiningCodeInputState) {
-                    final TextEditingController leagueCodeController = TextEditingController();
-                    Future.delayed(Duration.zero, () { // avoids "setState() or markNeedsBuild() called during build"
-                      showDialog(
-                        context: context,
-                        builder: (context) => _buildLeagueCodeInputDialog(context, leagueCodeController),
-                      );
-                    });
-                    return _buildInitialUI(context);
-                  } else if (state is LeagueJoiningSuccessState) {
-                    Future.delayed(Duration.zero, () { // avoids "setState() or markNeedsBuild() called during build"
-                      showDialog(
-                        context: context,
-                        builder: (context) => _buildSuccessDialog(state),
-                      );
-                    });
-                    return _buildInitialUI(context); // this is under the dialog
-                  } else if (state is LeagueJoiningFailedState) {
-                    Future.delayed(Duration.zero, () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => _buildFailedDialog(state),
-                      );
-                    });
-                    return _buildInitialUI(context);
-                  } else if (state is LeagueJoiningConfirmationSuccessState) {
-                    Future.delayed(Duration.zero, ()
-                    {
-                      showDialog(
-                        context: context,
-                        builder: (context) => _buildConfirmationSuccessDialog(),
-                      );
-                    });
-                    return _buildInitialUI(context);
-                  } else {
-                    return _buildInitialUI(context);
-                  }
-                },
-                listener: (context, state) {
-                  // Add any necessary logic based on state changes
-                },
-              ),
+              SizedBox(height: 50),
+              Text('Random Text'),
+              SizedBox(height: 20),
+              // JoinLeagueWidget(),
+              AddPointsWidget(),
             ],
           ),
         ),
       ],
-    );
-  }
-
-  Widget _buildInitialUI(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        BlocProvider.of<LeagueJoiningBloc>(context).add(JoinLeagueButtonClickedEvent());
-      },
-      child: const Text('Join League'),
-    );
-  }
-
-  // void _showDialog(Widget content) {
-  //   showDialog(
-  //     context: context,
-  //     builder: (context) => content,
-  //   );
-  // }
-
-  Widget _showLoadingDialog() {
-    return CircularProgressIndicator(backgroundColor: Colors.black.withOpacity(0.5),);
-  }
-
-  // Widget _showLeagueCodeInputDialog(BuildContext context) {
-  //   final TextEditingController leagueCodeController = TextEditingController();
-  //
-  //   return ElevatedButton(
-  //     onPressed: () {
-  //       _showDialog(_buildLeagueCodeInputDialog(context, leagueCodeController));
-  //     },
-  //     child: const Text('Join League'),
-  //   );
-  // }
-
-  Widget _buildLeagueCodeInputDialog(BuildContext context, TextEditingController leagueCodeController) {
-    return AlertDialog(
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text('Enter League Code'),
-          const SizedBox(height: 8.0),
-          TextFormField(
-            controller: leagueCodeController,
-            decoration: const InputDecoration(hintText: 'League Code'),
-          ),
-          const SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(); // Close the dialog
-                  BlocProvider.of<LeagueJoiningBloc>(context).add(CancelLeagueJoinEvent());
-                },
-                child: const Text('Cancel'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Trigger the league joining process
-                  BlocProvider.of<LeagueJoiningBloc>(context).add(
-                    CheckLeagueJoinPrerequisitesEvent(
-                      leagueCode: leagueCodeController.text,
-                    ),
-                  );
-                  Navigator.of(context).pop(); // Close the dialog
-                },
-                child: const Text('Join'),
-              ),
-            ],
-          ),
-        ],
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-    );
-  }
-
-  Widget _buildFailedDialog(LeagueJoiningFailedState state) {
-    return AlertDialog(
-      title: const Text('Error joining league'),
-      content: SingleChildScrollView(child: Text(state.message)),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Close the dialog
-            // BlocProvider.of<LeagueJoiningBloc>(context).add(CancelLeagueJoinEvent());
-          },
-          child: const Text('Ok'),
-        ),
-      ],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-    );
-  }
-
-  Widget _buildSuccessDialog(LeagueJoiningSuccessState state) {
-    return AlertDialog(
-      title: Text('League Name: ${state.leagueName}'),
-      content: Text('Points Needed to Join: ${state.pointsNeeded}'),
-      actions: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Close the dialog
-            BlocProvider.of<LeagueJoiningBloc>(context).add(CancelLeagueJoinEvent());
-          },
-          child: const Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            // Trigger the second cloud function to confirm join
-            BlocProvider.of<LeagueJoiningBloc>(context).add(ConfirmJoinLeagueEvent(leagueId: state.leagueId));
-            Navigator.of(context).pop(); // Close the dialog
-          },
-          child: const Text('Confirm Join'),
-        ),
-      ],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-    );
-  }
-
-  Widget _buildConfirmationSuccessDialog() {
-    return AlertDialog(
-      title: const Text('Success'),
-      content: const Text('You have successfully joined the league!'),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Close the dialog
-            BlocProvider.of<LeagueJoiningBloc>(context).add(CancelLeagueJoinEvent());
-          },
-          child: const Text('Ok'),
-        ),
-      ],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
     );
   }
 }
