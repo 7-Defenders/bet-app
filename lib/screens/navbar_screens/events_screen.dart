8d0bd9b2d4f69985edd1b331bbb2ad7326b0cf98
Franciszek Suszko <84635502+sunba23@users.noreply.github.com>
2023-12-12 20:48:34 +0100

    League widget (#12)
    * initial league widget created
    
    needs further work, but works for now
    
    * changes to widget height, deleted widget title and changed colors
    
    * changed location of the file
    
    ---------
    
    Co-authored-by: hermanica <maciek1250herman@gmail.com>

--- lib/screens/navbar_screens/events_screen.dart
+++ lib/screens/navbar_screens/events_screen.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:app/models/structure.dart'; 
+import 'package:app/components/leagues_screen/league_widget.dart';
 import 'package:http/http.dart' as http;
 
 class EventsScreen extends StatefulWidget {
@@ -20,3 +23,31 @@ class _EventsScreenState extends State<EventsScreen> {
     );
   }
 }
+
+// tested league widget here because this screen was empty
+
+// class _EventsScreenState extends State<EventsScreen> {
+//   // Create a mock list of leagues for demonstration purposes.
+//   final List<League> mockLeagues = [
+//     League(id: '1', name: 'Szymon', svgPath: 'lib/assets/images/league_logos/premier_league.svg'),
+//     League(id: '2', name: 'Dawid', svgPath: 'lib/assets/images/league_logos/premier_league.svg'),
+//     League(id: '3', name: 'Matto', svgPath: 'lib/assets/images/league_logos/premier_league.svg'),
+//     League(id: '4', name: 'SEMPUUU', svgPath: 'lib/assets/images/league_logos/premier_league.svg'),
+//     League(id: '5', name: 'Herman', svgPath: 'lib/assets/images/league_logos/premier_league.svg'),
+//     League(id: '6', name: 'Maciek', svgPath: 'lib/assets/images/league_logos/premier_league.svg'),
+//   ];
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         LeagueListWidget(
+//           leagues: mockLeagues,
+//           height: 300, 
+//         ),
+//       ],
+//     );
+//   }
+// }
+
