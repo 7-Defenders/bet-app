0dfcc726bf048cd2c5cc4f76f721f222cb8dd7f5
Mateusz Woźniak <mateusz.wozniak.0211@gmail.com>
2024-01-03 21:25:38 +0100

    Provisionary widget shapes

--- lib/components/events_screen/bet_preview.dart
+++ lib/components/events_screen/bet_preview.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'dart:math' as math;
+
 class BetPreviewWidget extends StatefulWidget {
   final String eventName;
   final String eventDetails;
@@ -36,14 +38,15 @@ class _BetPreviewWidgetState extends State<BetPreviewWidget> {
   @override
   Widget build(BuildContext context) {
     //final buttonStatesProvider = Provider.of<ButtonStatesProvider>(context);
-    final double containterWidth = MediaQuery.of(context).size.width * 0.9;
+    final double containerWidth = MediaQuery.of(context).size.width * 0.9;
     const double padding = 8.0;
     const SizedBox paddingBox = SizedBox(height: padding, width: padding);
+    final double oneWidth = (containerWidth - 2* padding) / 5;
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 6.0),
       child: Container(
-        width: containterWidth,
+        width: containerWidth,
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(30.0),
@@ -109,7 +112,7 @@ class _BetPreviewWidgetState extends State<BetPreviewWidget> {
                   children: widget.bets.entries.map((entry) {
                     return SizedBox(
                       width:
-                          (containterWidth - 4 * padding) / widget.bets.length,
+                          (containerWidth - 4 * padding) / widget.bets.length,
                       height: 38.0,
                       child: ElevatedButton(
                         onPressed: () {
@@ -156,11 +159,328 @@ class _BetPreviewWidgetState extends State<BetPreviewWidget> {
                     );
                   }).toList(),
                 ),
+                const SizedBox(height: 4),
+                Row(children: [
+                  Button1(onTap: (){print('1');}, width: oneWidth, height: 50),
+                  Button1X(onTap: (){print('1X');}, width: oneWidth, height: 50),
+                  ButtonX(onTap: (){print('X');}, width: oneWidth, height: 50),
+                  ButtonX2(onTap: (){print('X2');}, width: oneWidth, height: 50),
+                  Button2(onTap: (){print('2');}, width: oneWidth, height: 50),
+                ],),
+                ],),
               ],
             ),
-          ],
+        ),
+      );
+  }
+}
+
+class Button1 extends StatelessWidget {
+  double width;
+  double height;
+
+  Button1({super.key, required this.onTap, required this.width, required this.height});
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: CustomPaint(
+          painter: Shape1(),
+          child: Center(
+            child: Transform.translate(
+              offset: const Offset(15, 0),
+              child: const Icon(
+                Icons.send,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ),
       ),
     );
+  }
+}
+
+class Button1X extends StatelessWidget {
+  double width;
+  double height;
+
+  Button1X({super.key, required this.onTap, required this.width, required this.height});
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: CustomPaint(
+          painter: Shape1X(),
+          child: Center(
+            child: Transform.translate(
+              offset: const Offset(15, 0),
+              child: const Icon(
+                Icons.send,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonX extends StatelessWidget {
+  double width;
+  double height;
+
+  ButtonX({super.key, required this.onTap, required this.width, required this.height});
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: CustomPaint(
+          painter: ShapeX(),
+          child: Center(
+            child: Transform.translate(
+              offset: const Offset(15, 0),
+              child: const Icon(
+                Icons.send,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonX2 extends StatelessWidget {
+  double width;
+  double height;
+
+  ButtonX2({super.key, required this.onTap, required this.width, required this.height});
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: CustomPaint(
+          painter: ShapeX2(),
+          child: Center(
+            child: Transform.translate(
+              offset: const Offset(15, 0),
+              child: const Icon(
+                Icons.send,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Button2 extends StatelessWidget {
+  double width;
+  double height;
+
+  Button2({super.key, required this.onTap, required this.width, required this.height});
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: CustomPaint(
+          painter: Shape2(),
+          child: Center(
+            child: Transform.translate(
+              offset: const Offset(15, 0),
+              child: const Icon(
+                Icons.send,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Shape1 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.pink.shade400;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+    
+    const angle = math.pi / 8;
+    final fillWidth = size.height * math.tan(angle);
+
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width - fillWidth, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class Shape1X extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.pink.shade400;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+    
+    const angle = math.pi / 8;
+    final fillWidth = size.height * math.tan(angle);
+
+    final path = Path();
+    path.moveTo(fillWidth, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width - fillWidth, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(fillWidth, 0);
+    
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class ShapeX extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.pink.shade400;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+    
+    const angle = math.pi / 8;
+    final fillWidth = size.height * math.tan(angle);
+
+    final path = Path();
+    path.moveTo(fillWidth, 0);
+    path.lineTo(size.width - fillWidth, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(fillWidth, 0);
+    
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class ShapeX2 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.pink.shade400;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+    
+    const angle = math.pi / 8;
+    final fillWidth = size.height * math.tan(angle);
+
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width - fillWidth, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(fillWidth, size.height);
+    path.lineTo(0, 0);
+    
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class Shape2 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.pink.shade400;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+    
+    const angle = math.pi / 8;
+    final fillWidth = size.height * math.tan(angle);
+
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(fillWidth, size.height);
+    path.lineTo(0, 0);
+    
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
