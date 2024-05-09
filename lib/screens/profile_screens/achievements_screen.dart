1b8cf97b0399ee54096ffc122275ce34c6762208
franek <franeksu@gmail.com>
2024-05-09 17:53:39 +0200

    fl-up

--- lib/screens/profile_screens/achievements_screen.dart
+++ lib/screens/profile_screens/achievements_screen.dart
@@ -1,10 +1,13 @@
 import 'package:app/components/other/appbar/custom_appbar.dart';
+import 'package:app/components/other/nunito_text.dart';
 import 'package:app/models/user_data.dart';
 import 'package:app/providers/user_data_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,136 +21,6 @@ class AchievementsScreen extends StatefulWidget {
 enum AchievementType {
   betsWon,
   leaguesJoined,
-}
-
-Widget achievement(
-  BuildContext context,
-  String achievementName,
-  AchievementType type,
-  num? valueCurrent,
-  num valueMax,
-  String cosmeticName,
-) {
-  final bool isAchievementDone =
-      valueCurrent != null && valueCurrent >= valueMax;
-
-  return FutureBuilder<SharedPreferences>(
-    future: SharedPreferences.getInstance(),
-    builder: (context, snapshot) {
-      if (!snapshot.hasData) {
-        return const CircularProgressIndicator();
-      }
-
-      final prefs = snapshot.data!;
-      final bool isRewardCollected =
-          prefs.getBool('$achievementName achievement') ?? false;
-
-      return Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Column(
-          children: [
-            if (type == AchievementType.betsWon)
-              Text('Win $valueMax bets')
-            else
-              Text('Join $valueMax leagues'),
-            Text('$valueCurrent/$valueMax'),
-            ElevatedButton(
-              onPressed: isAchievementDone && !isRewardCollected
-                  ? () => giveReward(context, cosmeticName, achievementName)
-                  : null,
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) {
-                    if (!isAchievementDone) {
-                      return Colors.grey;
-                    } else if (isRewardCollected) {
-                      return Colors.yellow[700]!;
-                    }
-                    return Colors.green;
-                  },
-                ),
-              ),
-              child: Text(isRewardCollected ? 'Collected' : 'Collect'),
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
-
-List<Widget> achievementsList(UserData? userData, BuildContext context) {
-  return [
-    achievement(
-      context,
-      "Winner",
-      AchievementType.betsWon,
-      userData?.betsWon ?? 0,
-      10,
-      "blue boss",
-    ),
-    achievement(
-      context,
-      "Explorer",
-      AchievementType.leaguesJoined,
-      userData?.leaguesJoined ?? 0,
-      3,
-      "jester",
-    ),
-  ];
-}
-
-Future<void> giveReward(
-  BuildContext context,
-  String cosmeticName,
-  String achievementName,
-) async {
-  showDialog(
-    context: context,
-    builder: (context) {
-      return Center(
-        child: LoadingAnimationWidget.hexagonDots(
-          color: Theme.of(context).colorScheme.primary,
-          size: 55,
-        ),
-      );
-    },
-    barrierDismissible: false,
-    useRootNavigator: false,
-  );
-
-  final uid =
-      Provider.of<UserDataProvider>(context, listen: false).userData?.uid;
-  final uri = Uri.encodeFull(
-    'https://flask-vhn3gxevdq-ew.a.run.app/v1/users/$uid/cosmetics/$cosmeticName',
-  );
-  final response = await http.get(Uri.parse(uri));
-
-  if (context.mounted) {
-    Navigator.of(context).pop();
-  }
-
-  if (context.mounted) {
-    if (response.statusCode == 200) {
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setBool('$achievementName achievement', true);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("You recievsed a new cosmetic: $cosmeticName!"),
-          duration: const Duration(seconds: 2),
-        ),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text("Failed to give reward"),
-          duration: Duration(seconds: 2),
-        ),
-      );
-    }
-  }
 }
 
 class _AchievementsScreenState extends State<AchievementsScreen> {
@@ -170,9 +43,279 @@ class _AchievementsScreenState extends State<AchievementsScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: achievementsList(userData, context),
         ),
       ),
+    );
+  }
+}
+
+List<Widget> achievementsList(UserData? userData, BuildContext context) {
+  final achievements = [
+    AchievementWidget(
+      achievementName: "Winner",
+      type: AchievementType.betsWon,
+      valueCurrent: userData?.betsWon ?? 0,
+      valueMax: 10,
+      cosmeticName: "blue boss",
+    ),
+    AchievementWidget(
+      achievementName: "Explorer",
+      type: AchievementType.leaguesJoined,
+      valueCurrent: userData?.leaguesJoined ?? 0,
+      valueMax: 1,
+      cosmeticName: "romantic",
+    ),
+    AchievementWidget(
+      achievementName: "The Brave One",
+      type: AchievementType.leaguesJoined,
+      valueCurrent: userData?.leaguesJoined ?? 0,
+      valueMax: 3,
+      cosmeticName: "groovy",
+    ),
+    AchievementWidget(
+      achievementName: "Serious Player",
+      type: AchievementType.leaguesJoined,
+      valueCurrent: userData?.leaguesJoined ?? 0,
+      valueMax: 5,
+      cosmeticName: "purple madness",
+    ),
+  ];
+
+  final completedAchievements = achievements
+      .where((widget) =>
+          widget.valueCurrent != null &&
+          widget.valueCurrent! >= widget.valueMax)
+      .toList();
+
+  final uncompletedAchievements = achievements
+      .where((widget) =>
+          widget.valueCurrent == null || widget.valueCurrent! < widget.valueMax)
+      .toList();
+
+  return [
+    const SizedBox(height: 40),
+    Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: nunitoText(
+        'Uncompleted',
+        20,
+        FontWeight.bold,
+        const Color.fromARGB(255, 30, 30, 27),
+      ),
+    ),
+    GridView.count(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      children: uncompletedAchievements,
+    ),
+    Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: nunitoText(
+        'Completed',
+        20,
+        FontWeight.bold,
+        const Color.fromARGB(255, 30, 30, 27),
+      ),
+    ),
+    GridView.count(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      children: completedAchievements,
+    ),
+  ];
+}
+
+class AchievementWidget extends StatefulWidget {
+  final String achievementName;
+  final AchievementType type;
+  final num? valueCurrent;
+  final num valueMax;
+  final String cosmeticName;
+
+  const AchievementWidget({
+    required this.achievementName,
+    required this.type,
+    required this.valueCurrent,
+    required this.valueMax,
+    required this.cosmeticName,
+    super.key,
+  });
+
+  @override
+  _AchievementWidgetState createState() => _AchievementWidgetState();
+}
+
+class _AchievementWidgetState extends State<AchievementWidget> {
+  bool isRewardCollected = false;
+
+  Future<void> giveReward(
+    BuildContext context,
+    String cosmeticName,
+    String achievementName,
+  ) async {
+    final uid =
+        Provider.of<UserDataProvider>(context, listen: false).userData?.uid;
+    final uri = Uri.encodeFull(
+      'https://flask-vhn3gxevdq-ew.a.run.app/v1/users/$uid/cosmetics/$cosmeticName',
+    );
+    final response = await http.post(Uri.parse(uri));
+
+    if (context.mounted) {
+      if (response.statusCode == 200) {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setBool('$achievementName achievement', true);
+        setState(() {
+          isRewardCollected = true;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("You recievsed a new cosmetic: $cosmeticName!"),
+            duration: const Duration(seconds: 2),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Failed to give reward"),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isAchievementDone =
+        widget.valueCurrent != null && widget.valueCurrent! >= widget.valueMax;
+
+    return FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        }
+
+        final prefs = snapshot.data!;
+        isRewardCollected =
+            prefs.getBool('${widget.achievementName} achievement') ?? false;
+
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              padding: const EdgeInsets.all(10),
+              child: GestureDetector(
+                onTap: !isRewardCollected
+                    ? () => giveReward(
+                          context,
+                          widget.cosmeticName,
+                          widget.achievementName,
+                        )
+                    : null,
+                child: CircularPercentIndicator(
+                  radius: constraints.maxWidth * 0.36,
+                  animation: true,
+                  animationDuration: 1200,
+                  lineWidth: 15.0,
+                  percent: (widget.valueCurrent! / widget.valueMax) > 1
+                      ? 1
+                      : (widget.valueCurrent! / widget.valueMax),
+                  backgroundColor: const Color.fromARGB(150, 107, 162, 243),
+                  progressColor: isAchievementDone
+                      ? Colors.green
+                      : const Color.fromARGB(255, 242, 148, 44),
+                  center: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: !isAchievementDone 
+                        ? [
+                            nunitoText(
+                              widget.type == AchievementType.betsWon
+                                  ? "Win"
+                                  : "Join",
+                              14,
+                              FontWeight.normal,
+                              const Color.fromARGB(255, 30, 30, 27),
+                            ),
+                            nunitoText(
+                              "${widget.valueMax}",
+                              20,
+                              FontWeight.bold,
+                              const Color.fromARGB(255, 30, 30, 27),
+                            ),
+                            nunitoText(
+                              widget.type == AchievementType.betsWon
+                                  ? "Bets"
+                                  : "Leagues",
+                              14,
+                              FontWeight.normal,
+                              const Color.fromARGB(255, 30, 30, 27),
+                            ),
+                            if (!isAchievementDone)
+                              nunitoText(
+                                "${widget.valueCurrent!} / ${widget.valueMax}",
+                                12,
+                                FontWeight.normal,
+                                const Color.fromARGB(255, 30, 30, 27),
+                              )
+                            else
+                              const SizedBox(),
+                          ] 
+                          : isRewardCollected ? [
+                                nunitoText(
+                                  widget.type == AchievementType.betsWon
+                                      ? "win ${widget.valueMax} bets"
+                                      : "join ${widget.valueMax} leagues",
+                                  14,
+                                  FontWeight.normal,
+                                  const Color.fromARGB(255, 30, 30, 27),
+                                ),
+                                nunitoText(
+                                  widget.cosmeticName,
+                                  20,
+                                  FontWeight.bold,
+                                  const Color.fromARGB(255, 30, 30, 27),
+                                ),
+                                nunitoText(
+                                  "Collected",
+                                  14,
+                                  FontWeight.normal,
+                                  const Color.fromARGB(255, 30, 30, 27),
+                                ),
+                              ],
+                            : [
+                                nunitoText(
+                                  widget.type == AchievementType.betsWon
+                                      ? "win ${widget.valueMax} bets"
+                                      : "join ${widget.valueMax} leagues",
+                                  14,
+                                  FontWeight.normal,
+                                  const Color.fromARGB(255, 30, 30, 27),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => giveReward(
+                                    context,
+                                    widget.cosmeticName,
+                                    widget.achievementName,
+                                  ),
+                                  child: nunitoText(
+                                    "Claim",
+                                    20,
+                                    FontWeight.bold,
+                                    Colors.green,
+                                  ),
+                                ),
+                              ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
