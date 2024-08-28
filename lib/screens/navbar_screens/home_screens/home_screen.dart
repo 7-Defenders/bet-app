2fc5e2234cff0279c4ed2f5cfdeb5c5ebb3f4f55
Mateusz Woźniak <mateusz.wozniak.0211@gmail.com>
2024-08-28 11:24:04 +0200

    coming soon home panels

--- lib/screens/navbar_screens/home_screens/home_screen.dart
+++ lib/screens/navbar_screens/home_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app/components/other/appbar/custom_appbar.dart';
+import 'package:app/components/other/nunito_text.dart';
 import 'package:app/providers/user_data_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -22,32 +23,162 @@ class _HomeScreenState extends State<HomeScreen> {
     await FirebaseAuth.instance.signOut();
   }
 
+  Widget buildSection(
+    String title,
+    double usableWidth,
+    double cardHeight,
+    double cardWidth,
+    List<Widget> widgets,
+    {String? description,
+    bool vertical=false,
+    bool hidden=false,}
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 8,
+        ),
+        nunitoText(title, 18, FontWeight.bold, Colors.black),
+        if (description != null) nunitoText(description, 14, FontWeight.normal, Colors.grey) else const SizedBox(),
+        const SizedBox(
+          height: 8,
+        ),
+        Stack(
+          children: [
+            if (vertical) 
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: widgets,
+                ),
+              )
+            else 
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: widgets,),
+              ),
+            if (hidden)
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).primaryColor.withOpacity(0.7),
+                  ),
+                  width: usableWidth,
+                  height: cardHeight,
+                  child: Center(
+                    child: nunitoText('Coming soon', 18, FontWeight.bold, Colors.white),
+                  ),
+                ),
+              ),
+          ],
+        ),
+
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    
+    final usableWidth = MediaQuery.of(context).size.width * 0.9;
+    final cardWidth = usableWidth * 0.45;
+    final cardHeight = MediaQuery.of(context).size.height * 0.15;
     return Scaffold(
-      appBar: const CustomAppbar(
-        56,
-        null,
-        'Home',
-        null,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: nunitoText('Home', 26, FontWeight.bold, Colors.black),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text('Home Screen'),
-            ElevatedButton(
-              onPressed: () {
-                goHome2(context);
-              },
-              child: const Text('Go to Home 2'),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            width: usableWidth,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildSection(
+                  "Invites",
+                  usableWidth, //this has no impact?
+                  cardHeight,
+                  cardWidth,
+                  [
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                  ],
+                  hidden: true,
+                ),
+            
+                buildSection(
+                  "Game modes",
+                  usableWidth,
+                  cardHeight,
+                  cardWidth,
+                  [
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                  ],
+                  hidden: true,
+                  description: "Checkout other game modes we have on offer",
+                ),
+                buildSection(
+                  "Popular",
+                  usableWidth,
+                  cardHeight,
+                  cardWidth,
+                  [
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                    SizedBox(
+                      width: cardWidth,
+                      height: cardHeight,
+                      child: const Card(),
+                    ),
+                  ],
+                  description: "Most popular events today",
+                  vertical: true
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                logOutUser();
-              },
-              child: const Text('Log out'),
-            ),
-          ],
+          ),
         ),
       ),
     );
